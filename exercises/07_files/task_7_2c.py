e7c4ea2f2e75fa6336dbb66aca523b864ac8b765
henzelis <henzelis@gmail.com>
2020-05-11 14:41:26 +0300

    task 7.2c done

--- exercises/07_files/task_7_2c.py
+++ exercises/07_files/task_7_2c.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Задание 7.2c
@@ -15,5 +16,24 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+from sys import argv
+
+src = argv[1]
+dst = argv[2]
 
 ignore = ["duplex", "alias", "Current configuration"]
+
+with open(src) as cfg, open(dst, 'w') as clear_cfg: 
+    for line in cfg:
+        if line.startswith('!'):
+             pass
+        else:      
+            for word in ignore:
+                if word not in line:
+                    flag = 1 
+                else:
+                    flag = 0
+                    break
+            if(flag == 1):
+                print(line.rstrip())
+                clear_cfg.write(line)
