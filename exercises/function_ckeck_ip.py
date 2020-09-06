3f55b2054918b15ca76fd9edde1f7ea066613f2e
henzelis <henzelis@gmail.com>
2020-09-06 12:25:09 -0500

    new file added

---
+++ exercises/function_ckeck_ip.py
@@ -0,0 +1,21 @@
+#
+'''
+Модуль проверки IP-адреса
+'''
+
+import ipaddress
+
+def check_ip(ip):
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+ip1 = '10.1.1.1'
+ip2 = '1.1.1'
+
+print('Проверяем IP-адреса')
+print(ip1, check_ip(ip1))
+print(ip2, check_ip(ip2))
+
