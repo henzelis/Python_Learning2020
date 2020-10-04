328e8f36071a1463b613adbb64b333ab6681e3b8
henzelis <henzelis@gmail.com>
2020-10-04 07:41:33 -0500

    12.1 updated

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,14 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+def ping_ip(ip): 
+    print('Пингую', ip) 
+    result = subprocess.run('ping {} -c 5 -n'.format(ip),
+          shell=True, stdout=subprocess.PIPE,  
+          stderr=subprocess.PIPE, encoding='utf-8') 
+    if result.returncode == 0: 
+        return True 
+    else: 
+        return False 
+                                                         
+
