f44c0a85142b2a4b1e850c4ec94cf4637401e936
henzelis <henzelis@gmail.com>
2020-11-08 19:06:30 +0200

    Linkedinn files

---
+++ linkedin_graber/prmt_linkedin_grb.py
@@ -0,0 +1,5 @@
+q_search_query = 'site:linkedin.com/in/ AND "Java Developer" AND "Ukraine"'
+file_name = 'results_file.csv'
+
+linkedin_username = 'mail@linkedin.com'
+linkedin_password = 'YourPaSsWoRd'
